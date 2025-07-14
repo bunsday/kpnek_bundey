978373ed636c869b6aa677eb0638bba1a9aeb08e
bunsday <129835187+bunsday@users.noreply.github.com>
2025-07-14 17:41:46 +0700

    Update config.js
    update

--- config.js
+++ config.js
@@ -1,20 +1,13 @@
 const CONFIG = {
-    titleWeb: "Gửi crush của tớ",
-    introTitle: 'Crush yêu dấu',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Thoát web là chấp nhận yêu tớ rồi nhé!!!',
-    btnYes: 'Thích lắm <33',
-    btnNo: 'Không nha :3',
-    question: 'Trên thế giới hơn 7 tỉ người mà sao bạn lại yêu mình <3',
-    btnReply: 'Gửi cho bạn <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Mình biết mà 🥰. Yêu bạn nhiều nhiều 😘😘',
-    messDesc: 'Tối nay, mình qua đón đi chơi nha.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'https://facebook.com/dinhkaito' //link mess của các bạn. VD: /dinhkaito
+    titleWeb: "Gửi KPhuongNek",
+    introTitle: 'Xinh đẹp iu dấu',
+    introDesc: `Trái đất vốn lạ thường`,
+    btnIntro: '^^Hẹ hẹ^^',
+    title: 'Sắp đến dịp đi bỉn, để Trùn Bướng đưa em đi lấp đầy thêm tủ đồ nhak 🥰',
+    desc: 'Thoát web là chấp nhận một cách phũ phàng nhak!!!',
+    btnYes: 'Kèm bữa nướng thì được =)) ',
+    btnNo: 'iem ở nhà, không thèm đi bỉn vs anh :3',
+    messDesc: 'Tối mai, a đón đi nha.',
+    btnAccept: 'Em biết ròi <3',
+    messLink: 'https://www.facebook.com/nqtruongdst217/' //link mess của các bạn. VD: /dinhkaito
 }
