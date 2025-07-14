e99ba88731ad80318bfe8072a301ced6e10282f6
bunsday <129835187+bunsday@users.noreply.github.com>
2025-07-14 17:50:47 +0700

    Update config.js

--- config.js
+++ config.js
@@ -7,7 +7,11 @@ const CONFIG = {
     desc: 'Thoát web là chấp nhận một cách phũ phàng nhak!!!',
     btnYes: 'Kèm bữa nướng thì được =)) ',
     btnNo: 'iem ở nhà, không thèm đi bỉn vs anh :3',
-    messDesc: 'Tối mai, a đón đi nha.',
-    btnAccept: 'Em biết ròi <3',
+    question: 'Sao em lại đồng ý vậy?',
+    btnReply: 'Gửi cho bạn <3',
+    reply: 'Mỗi anh chọn được đồ cho iem',
+    mess: 'Em bị dính lời nguyền rồi =)))',
+    messDesc: 'Gòi mai anh đón đi mua đồ',
+    btnAccept: 'Em biết ròiiii',
     messLink: 'https://www.facebook.com/nqtruongdst217/' //link mess của các bạn. VD: /dinhkaito
 }
